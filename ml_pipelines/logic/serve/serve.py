7e249ec5bdc81203bf6e9929a479f1a58b2c2111
azuur <44506730+azuur@users.noreply.github.com>
2024-01-18 09:20:26 -0500

    Feature/serve (#5)
    * Add code for serving
    
    * Add TODO
    
    * update ci
    
    ---------
    
    Co-authored-by: adrian-zuur <adrian.zuur@globant.com>

--- ml_pipelines/logic/serve/serve.py
+++ ml_pipelines/logic/serve/serve.py
@@ -1,5 +1,51 @@
-def validate_sample(sample: dict):
-    x1_upper_bound = 5
-    if sample["X1"] > x1_upper_bound:
-        return False
-    return True
+import pandas as pd
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from sklearn.linear_model import LogisticRegression
+
+from ml_pipelines.logic.common.feature_eng import (
+    FeatureEngineeringParams,
+    transform_features,
+)
+from ml_pipelines.logic.common.model import predict
+
+
+class Point(BaseModel):
+    prediction_id: str
+    x1: float = Field(..., description="Value of x1, must be smaller than 5", lt=5)
+    x2: float
+
+
+def create_fastapi_app(
+    model: LogisticRegression, feature_eng_params: FeatureEngineeringParams
+):
+    app = FastAPI()
+
+    @app.get("/predict/")
+    async def _(inputs: Point | list[Point]):
+        """
+        Endpoint to predict probabilities for the given list of points.
+        :param points: List of points (x1, x2) (or single point)
+        :return: List of probability predictions (or single prediction)
+        """
+        try:
+            single = False
+            if isinstance(inputs, Point):
+                single = True
+                inputs = [inputs]
+            # Convert the input points to a numpy array
+            x_matrix = pd.DataFrame([{"X1": p.x1, "X2": p.x2} for p in inputs])
+            x_matrix = transform_features(x_matrix, feature_eng_params)
+
+            # Make predictions using the pre-trained model
+            predictions = predict(model, x_matrix).tolist()
+
+            if single:
+                predictions = predictions[0]
+
+            return {"predictions": predictions}
+
+        except Exception:
+            raise HTTPException(status_code=500, detail="Internal server error")
+
+    return app
